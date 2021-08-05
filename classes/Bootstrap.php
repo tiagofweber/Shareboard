4350a32e3a30203c2c02565cd4540462b0032476
Tiago Weber <webertiagof@gmail.com>
2021-08-05 19:42:22 -0300

    Instantiate the controller class

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -10,8 +10,6 @@ class Bootstrap
     {
         $this->request = $request;
 
-        // die(var_dump($request));
-
         if ($this->request['controller'] == "") {
             $this->controller = 'home';
         } else {
@@ -22,9 +20,27 @@ class Bootstrap
             $this->action = 'index';
         } else {
             $this->action = $this->request['action'];
-        }
+        }        
+    }
 
-        var_dump($this->request);
-        
+    public function createController()
+    {
+        if (class_exists($this->controller)) {
+            $parents = class_parents($this->controller);
+            if (in_array("Controller", $parents)) {
+                if (method_exists($this->controller, $this->action)) {
+                    return new $this->controller($this->action, $this->request);
+                } else {
+                    echo '<h1>Method does not exist.</h1>';
+                    return;
+                }
+            } else {
+                echo '<h1>Base controller not found.</h1>';
+                return;
+            }
+        } else {
+            echo '<h1>Controller class does not exist.</h1>';
+            return;
+        }
     }
 }
--- index.php
+++ index.php
@@ -4,3 +4,5 @@ require 'config.php';
 require 'classes/Bootstrap.php';
 
 $bootstrap = new Bootstrap($_GET);
+
+$controller = $bootstrap->createController();
