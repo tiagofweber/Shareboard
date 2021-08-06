088602ea6313be7e1d1572de73d83d68589f7c98
Tiago Weber <webertiagof@gmail.com>
2021-08-05 23:41:14 -0300

    Creating views

--- models/home.php
+++ models/home.php
@@ -1,6 +1,6 @@
 <?php
 
-class HomeModel
+class HomeModel extends Model
 {
     public function Index()
     {
--- models/share.php
+++ models/share.php
@@ -1,9 +1,11 @@
 <?php
 
-class ShareModel
+class ShareModel extends Model
 {
     public function Index()
     {
-        return;
+        $this->query('SELECT * FROM shares');
+        $rows = $this->resultSet();
+        return $rows;
     }
 }
