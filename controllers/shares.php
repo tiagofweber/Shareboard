74215634041828c459c173cc7236d38c04cced0e
Tiago Weber <webertiagof@gmail.com>
2021-08-09 21:32:03 -0300

    Access control for add shares

--- controllers/shares.php
+++ controllers/shares.php
@@ -10,6 +10,9 @@ class Shares extends Controller
 
     protected function add()
     {
+        if (! isset($_SESSION['is_logged_in'])) {
+            header('Location: ' . ROOT_URL . 'shares');
+        }
         $viewModel = new ShareModel();
         $this->returnView($viewModel->add(), true);
     }
